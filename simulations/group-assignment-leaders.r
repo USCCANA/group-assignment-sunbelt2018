1bd2dc021029f4e2a803033089655901cc43e04a
George Vega <g.vegayon@gmail.com>
2018-06-08 12:39:06 -0700

    Working on timeout expressions

--- simulations/group-assignment-leaders.r
+++ simulations/group-assignment-leaders.r
@@ -1,9 +1,28 @@
 library(GroupAssignment)
+library(igraph)
+
+options(error = utils::dump.frames())
 
 # Reading the data in
 dat_attributes <- readRDS("simulations/dat_attributes.rds")
 dat_networks   <- readRDS("simulations/dat_networks.rds")
 dat_nleaders   <- readRDS("simulations/dat_nleaders.rds")
+
+ga <- function(...) {
+  on.exit(setTimeLimit())
+  # setTimeLimit(elapsed = 10)
+  ans <- tryCatch({
+      setTimeLimit(elapsed = 10, transient = TRUE)
+      optimalAssignment(...)
+    },
+    error = function(e) {
+    # message("Sorry, it took too long")
+    # NA
+    e
+  })
+  # setTimeLimit()
+  ans
+}
 
 # Computing assignments
 group_assignment_results <- vector("list", length(dat_networks))
@@ -26,8 +45,8 @@ for (n in names(group_assignment_results)) {
     
     # G <- G + 1L
     G <- max(G) - G + 1
-    message("Starting group assignment algorithm in network ", n,"...", appendLF = FALSE)
-    group_assignment_results[[n]][[i]] <- optimalAssignment(
+    message("Starting group assignment algorithm in class ", n, " network ", i, "...", appendLF = FALSE)
+    group_assignment_results[[n]][[i]] <- ga(
       G,
       leaders      = dat_nleaders[i],
       minGroupSize = 1,
